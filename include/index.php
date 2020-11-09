321bb7b9df547f752ac1c8fb2e65543d750518e0
KingAlAmir <74156690+KingAlAmir@users.noreply.github.com>
2020-11-10 09:38:12 +1300

    style

--- include/index.php
+++ include/index.php
@@ -3,10 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <title>Include Excercise</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 <div id="header">
-    <?php include "page1.php"; ?>,
+    <?php include "menu.php"; ?>,
     login, pictures</div>
 <div id="main">Contents</div>
 <div id="footer">contact information</div>
