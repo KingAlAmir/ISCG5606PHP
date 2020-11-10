d82244cbb3e3787cf4bc8011dd0ff9726dfaf9c8
KingAlAmir <74156690+KingAlAmir@users.noreply.github.com>
2020-11-11 09:18:56 +1300

    Update menu.php

--- include/menu.php
+++ include/menu.php
@@ -1,6 +1,5 @@
 <ul>
     <li><a href="index.php">Home</a></li>
-    <li><a href="login.php">login</a></li>
     <li><a href="aboutUs.php">About us</a></li>
     <li><a href="Products.php">Products</a></li>
 
